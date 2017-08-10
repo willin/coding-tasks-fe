6eee54770c8b3a8ad6e416aab56789cebba131e1
solocao <solocao@126.com>
2017-08-10 16:46:17 +0800

    PC端 2乘2 grid显示

--- src/component/quadrant/quadrant.vue
+++ src/component/quadrant/quadrant.vue
@@ -1,46 +1,78 @@
 <template>
-  <div class="flex-grid">
-    <div class="grid-item">
-      1
+    <div class="flex-grid">
+        <div class="column">
+            <div class="index1">
+                重要 紧急
+            </div>
+            <div class="index2">
+                重要 不紧急
+            </div>
+        </div>
+        <div class="column">
+            <div class="index3">
+                不重要 紧急
+            </div>
+            <div class="index4">
+                不重要 不紧急
+            </div>
+        </div>
     </div>
-    <div class="grid-item">
-      2
-    </div>
-    <div class="grid-item">
-      3
-    </div>
-    <div class="grid-item">
-      4
-    </div>
-  </div>
 </template>
-
 <script>
-export default {
-
-};
+  export default {};
 </script>
 
 <style lang="stylus">
-main {
-  position: absolute;
-  height: 100%;
-  width: 100%;
-  top: 0;
-  padding-top: 70px;
-}
-.flex-grid {
-  display: flex;
-  flex-wrap: wrap;
-  min-height: 100%;
-}
-.grid-item {
-  margin: 1em;
-  border: 1px solid #000;
-  display: flex;
-  flex-basis: calc(50%);
-  justify-content: center;
-  flex-direction: column;
-}
+    main {
+        position: absolute;
+        height: 100%;
+        width: 100%;
+        top: 0;
+        padding-top: 70px;
+    }
+
+    .flex-grid {
+        width 100%
+        height 100%
+        background red
+        display flex
+        flex-direction row
+        justify-content space-between
+        flex-wrap wrap
+        .column {
+            width 50%
+            height 100%
+            .index1 {
+                background firebrick
+                width 100%
+                height 50%
+            }
+            .index2 {
+                background gold
+                width 100%
+                height 50%
+            }
+            .index3 {
+                background green
+                width 100%
+                height 50%
+            }
+            .index4 {
+                background moccasin
+                width 100%
+                height 50%
+            }
+        }
+
+    }
+
+    .grid-item {
+        margin: 1em;
+        border: 1px solid #000;
+        display: flex;
+        flex-basis: calc(50%);
+        justify-content: center;
+        flex-direction: column;
+    }
 </style>
 
