362af788d71c008797bfb7adf1195a6e889bd7dc
willin <willin@willin.org>
2017-08-11 14:24:55 +0800

    flexbox done

--- src/component/quadrant/quadrant.vue
+++ src/component/quadrant/quadrant.vue
@@ -1,75 +1,28 @@
 <template>
-    <div class="flex-grid">
-        <div class="column">
-            <div class="index1">
-                重要 紧急
-            </div>
-            <div class="index2">
-                重要 不紧急
-            </div>
-        </div>
-        <div class="column">
-            <div class="index3">
-                不重要 紧急
-            </div>
-            <div class="index4">
-                不重要 不紧急
-            </div>
-        </div>
+    <div class="flex">
+        <div class="box">1</div>
+        <div class="box">2</div>
+        <div class="box">3</div>
+        <div class="box">4</div>
     </div>
 </template>
 
-<style lang="stylus">
-    main {
-        position: absolute;
-        height: 100%;
-        width: 100%;
-        top: 0;
-        padding-top: 70px;
-    }
+<script>
+export default {
 
-    .flex-grid {
-        width 100%
-        height 100%
-        background red
-        display flex
-        flex-direction row
-        justify-content space-between
-        flex-wrap wrap
-        .column {
-            width 50%
-            height 100%
-            .index1 {
-                background firebrick
-                width 100%
-                height 50%
-            }
-            .index2 {
-                background gold
-                width 100%
-                height 50%
-            }
-            .index3 {
-                background green
-                width 100%
-                height 50%
-            }
-            .index4 {
-                background moccasin
-                width 100%
-                height 50%
-            }
-        }
+};
+</script>
 
-    }
+<style lang="stylus" scoped>
+.flex
+  height calc(100vh - 74px)
+  display flex
+  flex-flow column wrap
+  justify-content space-around
+  align-content space-around
+  align-items center
 
-    .grid-item {
-        margin: 1em;
-        border: 1px solid #000;
-        display: flex;
-        flex-basis: calc(50%);
-        justify-content: center;
-        flex-direction: column;
-    }
+.box
+  width 49%
+  height 49%
 </style>
-
