21e9192bea12daa8c2d31a3b00b4f8b06a4f5b33
willin <willin@willin.org>
2017-08-12 00:19:18 +0800

    task arr filter sort

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,5 +1,21 @@
 const getters = {
-  tasks: state => state.task,
+  tasks: (state) => {
+    const tasks = state.task.sort((prev, next) => (prev.deadline > next.deadline ? -1 : 1));
+    // eslint-disable-next-line no-proto
+    tasks.__proto__.where = function Where(where) {
+      return this.filter((task) => {
+        const keys = Object.keys(where);
+        for (let i = 0; i < keys.length; i += 1) {
+          const key = keys[i];
+          if (where[key] !== task[key]) {
+            return false;
+          }
+        }
+        return true;
+      });
+    };
+    return tasks;
+  },
   users: state => state.user,
   projects: state => state.project,
   labels: state => state.label,
