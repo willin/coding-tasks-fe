2a7e62dfc3522c687e6dd2f3b0a8bc367b366233
willin <willin@willin.org>
2017-08-12 18:58:13 +0800

    统计路由, 添加两个示例图表

--- src/component/statistics/statistics.vue
+++ src/component/statistics/statistics.vue
@@ -3,97 +3,60 @@
     <h1>
       Hello
     </h1>
-    <chart :options="options"></chart>
+    <component v-bind:is="currentView" :params="params"> </component>
   </div>
 </template>
 
 <script>
-import chart from 'vue-echarts/components/ECharts.vue';
-/* eslint-disable import/no-extraneous-dependencies */
-// echarts 位于 vue-echarts 的依赖项中
-import 'echarts/lib/chart/pie';
-import 'echarts/lib/component/polar';
-import 'echarts/lib/component/tooltip';
+import index from './default.vue';
+import team from './team.vue';
+import project from './project.vue';
+import user from './user.vue';
 
 export default {
   components: {
-    chart
+    index,
+    team,
+    project,
+    user
   },
   data() {
     return {
-      options: {
-        backgroundColor: '#2c343c',
-
-        title: {
-          text: 'Customized Pie',
-          left: 'center',
-          top: 20,
-          textStyle: {
-            color: '#ccc'
-          }
-        },
-
-        tooltip: {
-          trigger: 'item',
-          formatter: '{a} <br/>{b} : {c} ({d}%)'
-        },
-
-        visualMap: {
-          show: false,
-          min: 80,
-          max: 600,
-          inRange: {
-            colorLightness: [0, 1]
-          }
-        },
-        series: [
-          {
-            name: '访问来源',
-            type: 'pie',
-            radius: '55%',
-            center: ['50%', '50%'],
-            data: [
-              { value: 335, name: '直接访问' },
-              { value: 310, name: '邮件营销' },
-              { value: 274, name: '联盟广告' },
-              { value: 235, name: '视频广告' },
-              { value: 400, name: '搜索引擎' }
-            ].sort((a, b) => a.value - b.value),
-            roseType: 'radius',
-            label: {
-              normal: {
-                textStyle: {
-                  color: 'rgba(255, 255, 255, 0.3)'
-                }
-              }
-            },
-            labelLine: {
-              normal: {
-                lineStyle: {
-                  color: 'rgba(255, 255, 255, 0.3)'
-                },
-                smooth: 0.2,
-                length: 10,
-                length2: 20
-              }
-            },
-            itemStyle: {
-              normal: {
-                color: '#c23531',
-                shadowBlur: 200,
-                shadowColor: 'rgba(0, 0, 0, 0.5)'
-              }
-            },
-
-            animationType: 'scale',
-            animationEasing: 'elasticOut',
-            animationDelay() {
-              return Math.random() * 200;
-            }
-          }
-        ]
-      }
+      currentView: '',
+      params: []
     };
+  },
+  computed: {
+    view: {
+      get() {
+        return this.currentView;
+      },
+      set(param) {
+        let params = param[0];
+        if (params === undefined) {
+          this.currentView = 'index';
+          return;
+        }
+        params = params.split('/');
+        if (['team', 'project', 'user'].indexOf(params[0]) !== -1 && params.length > 1) {
+          this.currentView = params[0];
+          this.params = params;
+          return;
+        }
+        this.currentView = 'index';
+      }
+    }
+  },
+  beforeRouteEnter(to, from, next) {
+    next((vm) => {
+      // eslint-disable-next-line no-param-reassign
+      vm.view = to.params;
+    });
+  },
+  watch: {
+    $route() {
+      this.view = this.$route.params;
+    }
   }
 };
 </script>
